0fe058d4cd0078311994e65dbddb3e500eef698d
aelng <elnagmiali@gmail.com>
2025-01-04 20:38:29 -0500

    'fixed'

--- src/About.jsx
+++ src/About.jsx
@@ -1,19 +1,16 @@
 
 
 function About() {
-    return(
-        <>
-            <div className="flex flex-col" id="about">
-                <div className="mt-10 max-w-2xl mx-auto bg-[#181c3d] text-white rounded-lg">
-                    <b className="pl-3 pr-[21em] py-3 text-bold">C:\Users\ali\Desktop\about.txt</b>
-                </div>
-                <div className="max-w-xl mx-auto bg-[#0f1126] text-white rounded-lg">
-                    <p className="p-10 pl-6 pr-[4em] py-6 text-[1.2em]">Hello! I'm Ali, a current double degree student studying CS @ UW and BBA @ WLU.
-                        I think that programming is cool which is why I made this cool website that showcases my cool programming projects. (true story)</p>
-                </div>
-                
+    return (
+        <div className="flex flex-col text-white max-w-xl mx-auto" id="about">
+            <p className="py-1 font-bold bg-[#191d3f] rounded-t-lg p-4">C:\Users\ali\Desktop\about.txt</p>
+            <div className="bg-[#0f1126] rounded-b-lg p-4">
+                <p className="text-[1.2em]">
+                    Hello! I'm Ali, a current double degree student studying CS @ UW and BBA @ WLU.
+                    I think that programming is cool which is why I made this cool website that showcases my cool programming projects. (true story)
+                </p>
             </div>
-        </>
+        </div>
     );
 }
 
--- src/Me.jsx
+++ src/Me.jsx
@@ -3,8 +3,9 @@ import ali from "./assets/prophoto.jpg";
 function Me() {
     return (
         <>
-            <div className="flex justify-center my-[110px] text-white" id="home">
-                <img src={ali} className="w-[200px] h-auto rounded-2xl"></img>
+            <div className="flex flex-row justify-center my-[110px] text-white" id="home">
+                
+                <img src={ali} className="w-[200px] h-auto rounded-2xl text-center"></img>
                 <div className="flex flex-col justify-center max-w-[600px] ml-8">
                     <h1 className="font-bold text-3xl text-center leading-[5rem] ">Ali Elnagmi</h1>
                     <p className="text-md leading-tight text-center max-w-[300px]">Aspiring SWE who's always looking to build something new</p>

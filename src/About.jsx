cbbcbbc0c793548dc51e7d24bbf9081b7f82eb71
aelng <elnagmiali@gmail.com>
2025-01-04 22:46:57 -0500

    'fixed_about'

--- src/About.jsx
+++ src/About.jsx
@@ -6,7 +6,7 @@ function About() {
             <p className="py-1 font-bold bg-[#191d3f] rounded-t-lg p-4">C:\Users\ali\Desktop\about.txt</p>
             <div className="bg-[#0f1126] rounded-b-lg p-4">
                 <p className="text-[1.2em]">
-                    Hello! I'm Ali, a current double degree student studying CS @ UW and BBA @ WLU.
+                    Hello! I'm Ali, a current double degree student studying CS @ UWaterloo and BBA @ WLU.
                     I think that programming is cool which is why I made this cool website that showcases my cool programming projects. (true story)
                 </p>
             </div>

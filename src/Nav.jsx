d998238665efdbd0809b12da2f75cdcc2c2ac9c2
aelng <cravezofficial@gmail.com>
2025-05-11 17:06:11 -0400

    change hover color

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -18,9 +18,9 @@ function Nav() {
         <div className="nav">
             <div className = "bg-opacity-0 md:bg-[#202020] flex justify-center items-center text-white py-6 px-8 md:px-32">
                 <ul className = "hidden md:flex items-center gap-12 font-semibold text-base ">
-                    <li><a className="hover:text-[#121212] transition-colors duration-150 font-bold tracking-wide px-3 text-2xl" href="/#home">Home</a></li>
-                    <li><a className="hover:text-[#121212] transition-colors duration-150 font-bold tracking-wide px-3 text-2xl" href="/#about">About</a></li>
-                    <li><a className="hover:text-[#121212] transition-colors duration-150 font-bold tracking-wide px-3 text-2xl" href="/#projects">Projects</a></li>
+                    <li><a className="hover:text-[#424242] transition-colors duration-150 font-bold tracking-wide px-3 text-2xl" href="/#home">Home</a></li>
+                    <li><a className="hover:text-[#424242] transition-colors duration-150 font-bold tracking-wide px-3 text-2xl" href="/#about">About</a></li>
+                    <li><a className="hover:text-[#424242] transition-colors duration-150 font-bold tracking-wide px-3 text-2xl" href="/#projects">Projects</a></li>
                 </ul>
             </div>
                 <div className = "flex justify-end">
@@ -34,9 +34,9 @@ function Nav() {
                     <button className="text-white md:hidden focus:outline-none mr-10 absolute top-4 right-4" onClick={toggleMenu}>
                         <AiOutlineClose size={40}/>
                     </button>
-                    <li><a className="hover:text-[#121212] text-4xl transition-colors duration-150 font-bold tracking-wide px-3" href="/#home" onClick={toggleMenu}>Home</a></li>
-                    <li><a className="hover:text-[#121212] text-4xl transition-colors duration-150 font-bold tracking-wide px-3" href="/#about" onClick={toggleMenu}>About</a></li>
-                    <li><a className="hover:text-[#121212] text-4xl transition-colors duration-150 font-bold tracking-wide px-3" href="/#projects" onClick={toggleMenu}>Projects</a></li>
+                    <li><a className="hover:text-[#424242] text-4xl transition-colors duration-150 font-bold tracking-wide px-3" href="/#home" onClick={toggleMenu}>Home</a></li>
+                    <li><a className="hover:text-[#424242] text-4xl transition-colors duration-150 font-bold tracking-wide px-3" href="/#about" onClick={toggleMenu}>About</a></li>
+                    <li><a className="hover:text-[#424242] text-4xl transition-colors duration-150 font-bold tracking-wide px-3" href="/#projects" onClick={toggleMenu}>Projects</a></li>
                     </ul>
                 </div>
             </div>
